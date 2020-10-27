b975cabd6f7a0a025e2926e3fe0f1b3b22315758
Ivan Urra <ivan.urra.wd@gmail.com>
2020-10-27 20:05:56 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+console.log('JS connected');
+
+// getElementById
+
+let theCatDiv = document.getElementById('cat');
+console.log(theCatDiv); // <== what can you see in browser's console
+
+// element.innerHTML
+// TO ADD TEXT TO DOM USE "innerHTML" 
+theCatDiv.innerHTML = "I'm a cat";
+
+// element.style
+
+theCatDiv.style.backgroundColor = 'red';
+theCatDiv.style.border = '2px green solid';
+theCatDiv.style.fontSize = '50px';
+theCatDiv.style.marginTop = '30px';
+theCatDiv.style.paddingBottom = '30px';
+
+// .getElementsByClassName ()
+// let elements = document.getElementsByClassName(names);
+
+let mice = document.getElementsByClassName('mouse');
+console.log(mice); // <== HTMLCollection(3) [div.mouse, div.mouse, div.mouse]
+
+// Iterate over an HTML Collection
+// The HTMLCollection is an array-like object but is not an array. 
+// So we can’t use the array methods like forEach, map, push, etc.
+// To iterate over an HTMLCollection, we should use a for loop.
+// Another option we have is to turn our HTML collection into an array, using spread operator
+
+let miceArray = [...mice];
+
+console.log(miceArray); // <== [div.mouse, div.mouse, div.mouse]
+
+// Accessing by Tag Name
+// .getElementsByTagName()
+// let elements = document.getElementsByTagName(name);
+
+let divs = document.getElementsByTagName('div');
+console.log(divs); // <== [div#cat, div.mouse, div.mouse, div.mouse]
+
+// querySelector Accessing First Found Selector
+// let theFirstFoundElem = document.querySelector(selectors)
+//
+//
+
